0d41c0655332637114381b278190ecdd1405d202
corbin-coleman <corbincol@hotmail.com>
2017-02-09 05:51:00 +0000

    Serialize datetime object properly

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import datetime
 import uuid
 from models.__init__ import storage
@@ -9,19 +8,25 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         if args and type(args[0]) is dict:
+            self.created_at = datetime.datetime.strptime(args[0]['created_at'], '%Y-%m-%d %H:%H:S.%f')
+            if args[0][updated_at]:
+                self.updated_at = datetime.datetime.strptime(args[0]['updated_at'], '%Y-%m-%d %H:%H:S.%f')
             self.__dict__ = args[0]
         else:
-            self.created_at = datetime.datetime.now().isoformat()
+            self.created_at = datetime.datetime.now()
             self.id = str(uuid.uuid4())
             storage.new(self)
 
     def save(self):
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now()
         storage.save()
 
     def to_json(self):
-        self.__dict__.update({'__class__': self.__class__.__name__})
-        return self.__dict__
+        newdict = self.__dict__.copy()
+        newdict.update({'__class__': self.__class__.__name__})
+        newdict.update({'updated_at': str(self.updated_at)})
+        newdict.update({'created_at': str(self.created_at)})
+        return newdict
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
