f723932ee3571bd79505cd8ca55fe44f17c1d3f9
Kina Lee <kina.lee@holbertonschool.com>
2017-02-10 01:09:01 +0000

    classes

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+BaseModel = __import__('base_model').BaseModel
+
+class State(BaseModel):
+
+    def __init__(self):
+        name = ""
---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+
+    def __init__(self):
+        BaseModel.__init__(self)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
