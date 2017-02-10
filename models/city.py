ee0c721a9666805bc2b515eb3014731000ae12f3
corbin-coleman <corbincol@hotmail.com>
2017-02-10 05:50:53 +0000

    Run BaseModel.__init__ with proper dictionary or not

--- models/city.py
+++ models/city.py
@@ -5,6 +5,10 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if args and type(args[0]) is dict:
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
         state_id = ""
         name = ""
--- models/review.py
+++ models/review.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-BaseModel = __import__('base_model').BaseModel
+from models.base_model import BaseModel
 
 
 class Review(BaseModel):
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if args and type(args[0]) is dict:
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
         place_id = ""
         user_id = ""
         text = ""
