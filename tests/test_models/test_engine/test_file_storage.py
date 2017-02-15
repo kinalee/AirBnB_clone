1f946a5a93737f8a9efc2b230c7c7fb06877a572
Kina Lee <kina.lee@holbertonschool.com>
2017-02-15 12:43:05 +0000

    file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,6 +9,15 @@ from models.engine import file_storage
 class FileStorageTest(TestCase):
     """ test case class for FileStorage """
 
+    def setUp(self):
+        self.storage = file_storage.FileStorage()
+
+    def tearDown(self):
+        del self.storage
+
+    def test_all(self):
+        self.assertTrue(type(self.storage.all()) is dict)
+
 
 if __name__ == '__main__':
     unittst.main()
