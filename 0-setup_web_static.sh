06e361e0382e10b08420858fb5be83f9bf3b86e7
Kina Lee <kina.lee@holbertonschool.com>
2017-03-16 04:53:43 +0000

    sudo tee

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir - p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
-sudo echo "<html><head></head><body>Hello World</body></html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Hello World</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "39a\ \n\tlocation /hbtn_static {\n\t alias /data/web_static/current/hbnb_static;\n\t}\n" /etc/nginx/sites-enabled/default
