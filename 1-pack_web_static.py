3876e4bcafab12a8662d82c81cb76344734366ee
Kina Lee <kina.lee@holbertonschool.com>
2017-03-16 06:48:53 +0000

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the web_static folder
+"""
+import os
+from fabric.api import *
+from datetime import datetime
+
+def do_pack():
+    name = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    local('tar -cvzf "versions/web_static_%s.tgz" ./web_static' % name)
