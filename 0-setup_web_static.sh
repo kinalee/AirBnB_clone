f3aa3ad26d97cfafa40f160a7ca4652836833a58
Kina Lee <kina.lee@holbertonschool.com>
2017-03-16 05:34:54 +0000

    hbnb

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<html><head></head><body>Hello World</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Testing" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "39a\ \tlocation /hbtn_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo sed -i "39a\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
