c5dd9af9f6f6769adcb41f9f380fa0a838f094e9
Kina Lee <kina.lee@holbertonschool.com>
2017-03-16 05:14:39 +0000

    /

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/releases/test/
 echo "<html><head></head><body>Hello World</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
